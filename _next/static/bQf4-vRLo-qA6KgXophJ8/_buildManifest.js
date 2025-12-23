a020c2d58694ec9c4cc9e7e813f372a89223cb71
Tiago Ribeiro <tiagoribeiro@Mac.fritz.box>
2025-12-23 21:50:31 +0100

    revert: Remove hero background image

---
+++ _next/static/bQf4-vRLo-qA6KgXophJ8/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/engage//_next/:path+",
+        "destination": "/engage/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
